1e88e8e0dd6931258c84770fb685725b85df913c
kudrov <d@rr.ty>
2020-02-24 22:51:03 +0300

    Empty template

---
+++ src/components/TourBriefCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-card
+    class="ma-2"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      :src='tourInfo.picture'
+    >
+      <v-card-title>{{ tourInfo.title }}</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pb-0">{{ tourInfo.subtitle }}</v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div>Whitehaven Beach</div>
+
+      <div>Whitsunday Island, Whitsunday Islands</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="orange"
+        text
+      >
+        Share
+      </v-btn>
+
+      <v-btn
+        color="orange"
+        text
+        @click="toConsole"
+      >
+        Explore
+      </v-btn>
+    </v-card-actions>
+  </v-card
+    class="ma-2">
+</template>
+
+<script>
+  export default {
+    name: 'TourBriefCard',
+    props: ['tourInfo'],
+    data: () => ({
+      // loading: false,
+      // selection: 1,
+    }),
+
+    methods: {
+      toConsole () {
+        console.log('Info: ')
+        console.log(this.tourInfo)
+      },
+    },
+  }
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <div v-for="tour of tours" v-bind:key="tour.id">
+<!--      <v-flex class="pa-4">-->
+        <tour-brief-card :tourInfo="tour"/>
+<!--      </v-flex>-->
+    </div>
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  import TourBriefCard from '@/components/TourBriefCard.vue'
+
+  export default {
+    name: 'home',
+    components: {
+      TourBriefCard,
+    },
+    data: () => ({
+      'tours': [{
+        id: 0,
+        title: 'Top 10 Australian beaches',
+        subtitle: 'Number 10',
+        picture: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg',
+      },
+        {
+          id: 1,
+          title: 'City Tour',
+          subtitle: 'Number 1',
+          picture: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg',
+        },
+        {
+          id: 2,
+          title: 'Peterhof',
+          subtitle: 'Number 2',
+          picture: 'https://cdn.vuetifyjs.com/images/cards/docks.jpg',
+        }],
+    }),
+  }
+</script>
